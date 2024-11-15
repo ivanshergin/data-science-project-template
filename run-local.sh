e13f3399794634fd5dc7365d99bdcccc9f738513
ivanshergin <po4tashergina@yandex.ru>
2024-11-15 20:51:08 +0300

    fix: add project name for docker compose

--- run-local.sh
+++ run-local.sh
@@ -4,4 +4,4 @@ docker swarm init
 docker network create --attachable --driver=overlay ds_network
 
 
-docker-compose --name ds-project-template --env-file ./env.source -f ./docker/docker-compose.yml up --build
+docker-compose --project-name ds_project_template --env-file ./env.source -f ./docker/docker-compose.yml up --build
