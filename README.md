d9b14642b1499a857c5783a64260d3cfe19faa3f
ivanshergin <po4tashergina@yandex.ru>
2025-03-30 15:35:53 +0300

    feat: add mongo container

--- README.md
+++ README.md
@@ -5,6 +5,8 @@
     - postgres:15.3
     - ghcr.io/mlflow/mlflow:v2.18.0rc0
     - minio/minio:RELEASE.2024-11-07T00-52-20Z
+    - mongo:7.0.18-jammy
+    - mongo-express:1.0.2-20-alpine3.19
 
 ## Libs/ tools/ containers to add:
 
--- run-local.sh
+++ run-local.sh
@@ -1,7 +1,15 @@
 #/bin/bash
+ENV_FILENAME=".env"
 
 docker swarm init
 docker network create --attachable --driver=overlay ds_network
 
+docker volume create ds_postgres_data
+docker volume create ds_mlflow_data
+docker volume create ds_minio_data
+docker volume create ds_mongo_data
 
-docker-compose --project-name ds_project_template --env-file ./.env -f ./docker/docker-compose.yml up --build
+docker-compose --project-name ds_project_template \
+               --env-file $ENV_FILENAME  \
+               -f ./docker/docker-compose.yml \
+               up --build
